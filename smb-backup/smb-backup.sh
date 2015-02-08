8652effd346750432de5024749a71598babd37bf
Hakan Bayındır <hakan@bayindir.org>
2015-02-08 23:16:40 +0200

    Insert proper GNU/GPLv3 Header

--- smb-backup/smb-backup.sh
+++ smb-backup/smb-backup.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-###############################################################
-# SMB Backup - A small tool to backup remote SMB/CIFS shares. #
-# Developed by Hakan Bayindir <hakan@bayindir.org>            #
-# Licensed GNU/GPLv3                                          #
-###############################################################
+# SMB Backup - A small tool to backup remote SMB/CIFS shares.
+# Copyright (C) 2015  Hakan Bayindir <hakan@bayindir.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 # Exit on any error, don't make me write my own error handling
 set -e
